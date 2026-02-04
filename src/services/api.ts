1145c4af725ab9b618178e4216313817f8e6054e
sujancolab <sujancolab@gmail.com>
2026-02-04 23:05:40 +0530

    fixed

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:8000/api",
+  baseURL: "https://unistar-production.tech-trico.com/api",
 });
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
